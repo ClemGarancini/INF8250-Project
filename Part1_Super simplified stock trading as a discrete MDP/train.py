93be12a5ee897a80c6349cffe10d312169f145b9
SilverLightn <hipopotam495@gmail.com>
2023-12-13 21:28:42 -0500

    Added array for Jesse

--- Part1_Super simplified stock trading as a discrete MDP/train.py
+++ Part1_Super simplified stock trading as a discrete MDP/train.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def train_agent(env, agent, num_episodes):
     rewards_per_episode = []
+    q_values_array = np.zeros((num_episodes, 11, 6, 6, 2, 2, 4)) 
 
     for episode in range(num_episodes):
         state_index = env.reset()  # Get the initial state index
@@ -12,6 +14,12 @@ def train_agent(env, agent, num_episodes):
             action = agent.choose_action(state_index)
             next_state_index, reward, done = env.step(action)  # next_state_index is directly obtained here
             agent.learn(state_index, action, reward, next_state_index)
+
+            state_ = env.convert_index_to_state(state_index)
+            q_values_array[episode, 
+                           state_[0], state_[1], state_[2], state_[3], state_[4], 
+                           action] = agent.q_table[state_index, action]
+
             state_index = next_state_index
             total_rewards += reward
 
@@ -24,4 +32,4 @@ def train_agent(env, agent, num_episodes):
     plt.ylabel('Total Reward')
     plt.show()
 
-    return rewards_per_episode
+    return rewards_per_episode, q_values_array
