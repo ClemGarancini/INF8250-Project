ef0964a1d181f2aa5855c9394f692b0ea3d3f388
SilverLightn <hipopotam495@gmail.com>
2023-12-14 13:39:32 -0500

    Added array for Jesse

--- Part2_DQN/env.py
+++ Part2_DQN/env.py
@@ -1,20 +1,17 @@
 import numpy as np
 import pandas as pd
 
-
 class StockTradingEnvironmentDQN:
     def __init__(self, pepsi_file, cola_file):
         self.pepsi_data = pd.read_csv(pepsi_file)
         self.cola_data = pd.read_csv(cola_file)
 
-        self.state_dimensions = (
-            5  # balance, shares of Pepsi, shares of Cola, trend of Pepsi, trend of Cola
-        )
+        self.state_dimensions = 5  # balance, shares of Pepsi, shares of Cola, trend of Pepsi, trend of Cola
         self.action_space = 4  # Sell all, Hold, Buy Pepsi, Buy Cola
 
         self.state = np.array([10000.0, 0, 0, 0, 0])  # Initial state
         self.current_step = 0
-        self.previous_portfolio_value = self._calculate_portfolio_value()
+        self.previous_portfolio_value = self._calculate_portfolio_value() 
 
     def step(self, action):
         self._trade(action)
@@ -30,9 +27,7 @@ class StockTradingEnvironmentDQN:
         # More sophisticated reward functions can be used
         current_portfolio_value = self._calculate_portfolio_value()
         reward = current_portfolio_value - self.previous_portfolio_value
-        self.previous_portfolio_value = (
-            current_portfolio_value  # Update the previous portfolio value
-        )
+        self.previous_portfolio_value = current_portfolio_value  # Update the previous portfolio value
         return reward
 
     def _calculate_portfolio_value(self):
@@ -51,12 +46,12 @@ class StockTradingEnvironmentDQN:
             shares_pepsi, shares_cola = 0, 0
         elif action == 2:  # Buy Pepsi
             quantity = balance / pepsi_price
-            if quantity > 0:
+            if quantity > 0:                
                 shares_pepsi += quantity
                 balance -= quantity * pepsi_price
         elif action == 3:  # Buy Cola
             quantity = balance / cola_price
-            if quantity > 0:
+            if quantity > 0:   
                 shares_cola += quantity
                 balance -= quantity * cola_price
 
@@ -71,14 +66,10 @@ class StockTradingEnvironmentDQN:
         self.state[4] = cola_trend
 
     def _get_stock_price(self, step, stock_data):
-        return stock_data.iloc[step]["Close"]
+        return stock_data.iloc[step]['Close']
 
     def _get_stock_trend(self, step, stock_data):
-        return (
-            1
-            if stock_data.iloc[step]["Close"] > stock_data.iloc[step - 1]["Close"]
-            else 0
-        )
+        return 1 if stock_data.iloc[step]['Close'] > stock_data.iloc[step - 1]['Close'] else 0
 
     def reset(self):
         self.state = np.array([10000.0, 0, 0, 0, 0])  # Reset to initial state
--- Part2_DQN/train.py
+++ Part2_DQN/train.py
@@ -2,13 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 from collections import deque
 import random
+import torch
 
-def train_dqn_agent(env, agent, num_episodes, batch_size=32):
+def train_dqn_agent(env, agent, num_episodes, batch_size=32, state_bins=10):
     rewards_per_episode = []
 
+    # Define the discretization bins for each state dimension
+    balance_bins = np.linspace(0, 20000, state_bins)
+    shares_bins = np.linspace(0, 100, state_bins)
+    trend_bins = [0, 1]  # Since trend is binary
+
+    q_values_sample = np.zeros((num_episodes, state_bins, state_bins, state_bins, state_bins, state_bins, env.action_space))
+
     for episode in range(num_episodes):
-        state = env.reset()
-        state = np.reshape(state, [1, env.state_dimensions])  # Reshape for neural network compatibility
+        state_ = env.reset()
+        state = np.reshape(state_, [1, env.state_dimensions])  # Reshape for neural network compatibility
         total_rewards = 0
 
         done = False
@@ -18,6 +26,23 @@ def train_dqn_agent(env, agent, num_episodes, batch_size=32):
             next_state = np.reshape(next_state, [1, env.state_dimensions])
 
             agent.remember(state, action, reward, next_state, done)  # Remember the experience
+            
+                        # Discretize the state
+            discretized_state = [
+                np.digitize(state_[0], balance_bins) - 1,  # Balance
+                np.digitize(state_[1], shares_bins) - 1,   # Shares of Pepsi
+                np.digitize(state_[2], shares_bins) - 1,   # Shares of Cola
+                int(state_[3]),  # Trend of Pepsi
+                int(state_[4])   # Trend of Cola
+            ]
+
+            # Get Q-values and update the sample array
+            state_tensor = torch.from_numpy(state_).float().unsqueeze(0)
+            with torch.no_grad():
+                q_values = agent.model(state_tensor).numpy()[0]
+            q_values_sample[episode, discretized_state[0], discretized_state[1], discretized_state[2], discretized_state[3], discretized_state[4], action] = q_values[action]
+
+            
             state = next_state
             total_rewards += reward
 
@@ -35,4 +60,4 @@ def train_dqn_agent(env, agent, num_episodes, batch_size=32):
     plt.ylabel('Total Reward')
     plt.show()
 
-    return rewards_per_episode
+    return rewards_per_episode, q_values_sample
